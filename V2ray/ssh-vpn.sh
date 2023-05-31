084c4886f42b04cb9e582d7f603ec24455be5f8b
Exe303 <127712981+Exe303@users.noreply.github.com>
2023-05-31 18:17:06 +0700

    Update ssh-vpn.sh

--- V2ray/ssh-vpn.sh
+++ V2ray/ssh-vpn.sh
@@ -237,7 +237,7 @@ netfilter-persistent reload
 
 # download script
 cd /usr/bin
-wget -O add-host "https://${mdxvpn}/addhost.sh"
+wget -O addhost "https://${mdxvpn}/addhost.sh"
 wget -O menu "https://${mdxvpn}/menu.sh"
 wget -O usernew "https://raw.githubusercontent.com/Exe303/Bless/main/V2ray/usernew.sh"
 wget -O trial "https://raw.githubusercontent.com/Exe303/Bless/main/V2ray/trial.sh"
