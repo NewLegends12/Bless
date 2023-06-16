6db7f1a877791bf78fd7f7e8da8fb66daefec9a3
Exe303 <127712981+Exe303@users.noreply.github.com>
2023-06-16 12:53:57 +0700

    Add files via upload

---
+++ xray.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#Script By Mardhex
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+mkdir /var/lib/premium-script;
+echo "IP=" >> /var/lib/premium-script/ipvps.conf
+#install ssh ovpn
+wget https://raw.githubusercontent.com/Exe303/Bless/main/Ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+#Install Script Add Domain
+wget https://raw.githubusercontent.com/Exe303/Bless/main/Xray/m-xray.sh && chmod +x m-xray.sh && ./m-xray.sh
+wget https://raw.githubusercontent.com/Exe303/Bless/main/Backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+rm -f /root/ssh-vpn.sh
+rm -f /root/m-xray.sh
+rm -f /root/set-br.sh
+cat <<EOF> /etc/systemd/system/autosett.service
+[Unit]
+Description=autosetting
+Documentation=https://mdxcloud.net
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash /etc/set.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable autosett
+wget -O /etc/set.sh "https://raw.githubusercontent.com/Exe303/Bless/main/Ssh/set.sh"
+chmod +x /etc/set.sh
+history -c
+echo "1.2" > /home/ver
+clear
+echo " "
+echo " Auto Installer Sukses Terinstall "
+echo " "
+echo "==========A̳U̳T̳O̳ ̳S̳C̳R̳I̳P̳T̳ ̳B̳Y̳ ̳M̳D̳X̳T̳U̳N̳N̳E̳L̳=========" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - Vmess Tls               : 443"  | tee -a log-install.txt
+echo "   - Vmess NonTls            : 80"  | tee -a log-install.txt
+echo "   - Vless Tls               : 888"  | tee -a log-install.txt
+echo "   - Vless Non Tls           : 880"  | tee -a log-install.txt
+echo "   - Trojan                  : 8443"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   - Dev/Main                : KANG MARDHEX"  | tee -a log-install.txt
+echo "   - Telegram                : t.me/mdxdev"  | tee -a log-install.txt
+echo "   - Instagram               : *"  | tee -a log-install.txt
+echo "   - Whatsapp                : *"  | tee -a log-install.txt
+echo "   - Facebook                : *"  | tee -a log-install.txt
+echo "------------------A̳U̳T̳O̳ ̳S̳C̳R̳I̳P̳T̳ ̳B̳Y̳ ̳M̳D̳X̳T̳U̳N̳N̳E̳L̳-----------------" | tee -a log-install.txt
+echo ""
+echo " Server Reboot 10 Detik"
+sleep 10
+rm -f xcorev.sh
+reboot
