88938cd3cfed655e1aa6881e8496a792bce83a6a
Exe303 <127712981+Exe303@users.noreply.github.com>
2023-07-31 17:12:13 +0700

    Add files via upload

--- xcorev.sh
+++ xcorev.sh
@@ -7,12 +7,11 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
 #install ssh ovpn
-wget https://raw.githubusercontent.com/Exe303/Bless/main/V2ray/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+#wget https://raw.githubusercontent.com/Exe303/Bless/main/V2ray/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 #Install Script Add Domain
 wget https://raw.githubusercontent.com/Exe303/Bless/main/V2ray/mx-ray.sh && chmod +x mx-ray.sh && screen -S v2ray ./mx-ray.sh
-#Install AutoBackup
 wget https://raw.githubusercontent.com/Exe303/Bless/main/Backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
-rm -f /root/ssh-vpn.sh
+#rm -f /root/ssh-vpn.sh
 rm -f /root/mx-ray.sh
 rm -f /root/set-br.sh
 cat <<EOF> /etc/systemd/system/autosett.service
@@ -43,11 +42,11 @@ echo "" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - Vmess Tls               : 8443"  | tee -a log-install.txt
+echo "   - Vmess Tls               : 443"  | tee -a log-install.txt
 echo "   - Vmess NonTls            : 80"  | tee -a log-install.txt
 echo "   - Vless Tls               : 888"  | tee -a log-install.txt
 echo "   - Vless Non Tls           : 880"  | tee -a log-install.txt
-echo "   - Trojan                  : 443"  | tee -a log-install.txt
+echo "   - Trojan                  : 8443"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
@@ -59,7 +58,7 @@ echo "   - Whatsapp                : *"  | tee -a log-install.txt
 echo "   - Facebook                : *"  | tee -a log-install.txt
 echo "------------------A̳U̳T̳O̳ ̳S̳C̳R̳I̳P̳T̳ ̳B̳Y̳ ̳M̳D̳X̳T̳U̳N̳N̳E̳L̳-----------------" | tee -a log-install.txt
 echo ""
-echo " Server Reboot 5 detik"
-sleep 5
+echo " Server Reboot 10 Detik"
+sleep 10
 rm -f xcorev.sh
 reboot
